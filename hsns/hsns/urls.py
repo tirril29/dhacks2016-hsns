82b18ff99309e765e91b043e29ba81c13e2ed641
koprty <koprty@gmail.com>
2016-01-16 18:36:17 -0500

    added basic django with app

--- hsns/hsns/urls.py
+++ hsns/hsns/urls.py
@@ -1,4 +1,4 @@
-"""hacksite URL Configuration
+"""hsns URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- hsns/hsns/wsgi.py
+++ hsns/hsns/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hacksite project.
+WSGI config for hsns project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hacksite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hsns.settings")
 
 application = get_wsgi_application()
