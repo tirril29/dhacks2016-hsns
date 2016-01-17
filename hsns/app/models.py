9f02662545cd1102ce0cf50ae8da9bd709816555
Thomas Hsu <tomdenotela@gmail.com>
2016-01-16 19:44:04 -0500

    Added models.

--- hsns/app/models.py
+++ hsns/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Hackathon(models.model):
+	name = models.CharField(max_length = 30)
+
+class User(models.model):
+	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+	email_address = models.EmailField()
+	password = models.CharField(max_length = 30)
+
+class Post(models.model):
+	Hackathon = models.ForeignKey(Hackathon)
+	text = charfield(1024)
+	user = models.ForeignKey(User)
+	members = models.ManyToMany(User)
