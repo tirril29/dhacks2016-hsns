6020ccf02e873cefde21794cd0b0e0189beaa76c
Thomas Hsu <tomdenotela@gmail.com>
2016-01-16 22:58:09 -0500

    Adding forms.

---
+++ hsns/app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+import models
+
+class Login(forms.Form):
+    email_address = forms.EmailField()
+    password = forms.CharField()
+
+class Register(forms.Form):
+	first_name = Form.CharField(help_text = "Enter your first name. ")
+	last_name = Form.CharField(help_text = "Enter your last name. ")
+	email_address = Form.EmailField(help_text = "Enter your email address. ")
+	password = Form.CharField(help_text = "Enter your totally sercure password. ") # HELLA INSECURE
+
