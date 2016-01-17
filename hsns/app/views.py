849f2b6643eea847be3674c9fb58384355cc18e3
koprty <koprty@gmail.com>
2016-01-16 19:12:12 -0500

    simple routing w/o html files

--- hsns/app/views.py
+++ hsns/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+def index(request):
+    #return render(request, 'template/index.html');
+    return HttpResponse("Index Page");
+
+
+def hackathon(request):
+    return HttpResponse("About Page");
--- hsns/hsns/urls.py
+++ hsns/hsns/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', 'app.views.hackathon'),
+    url(r'^$',include("app.urls")),
+    
+
+
 ]
